3c185871f0d809def2f90ec8ccb60f793cd9db02
Yasutoshi Ito <hibari100206@gmail.com>
2017-05-24 11:26:59 +0900

    test commit

--- MDprg_v0.2.0/src/main.c
+++ MDprg_v0.2.0/src/main.c
@@ -17,28 +17,31 @@ int main()
   // at high speed.
 
 	GPIO_InitTypeDef init_buf;
-		RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
-		RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
-		RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1,ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1,ENABLE);
 
-		/*GPIO setting*/
-		/*PORTB*/
-		GPIO_StructInit(&init_buf);
-		init_buf.GPIO_Pin &= 0x0003;	//0,1Çê›íË
-		init_buf.GPIO_Mode = GPIO_Mode_OUT;
-		GPIO_Init(GPIOB,&init_buf);
-		/*PORTA*/
-		GPIO_StructInit(&init_buf);
-		init_buf.GPIO_Pin &= 0x0010;	//4Çê›íË
-		init_buf.GPIO_Mode = GPIO_Mode_OUT;
-		GPIO_Init(GPIOA,&init_buf);
-		/*End etting*/
+	/*GPIO setting*/
+	/*PORTB*/
+	GPIO_StructInit(&init_buf);
+	init_buf.GPIO_Pin &= 0x0003;	//0,1Çê›íË
+	init_buf.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(GPIOB,&init_buf);
+	/*PORTA*/
+	GPIO_StructInit(&init_buf);
+	init_buf.GPIO_Pin &= 0x0010;	//4Çê›íË
+	init_buf.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(GPIOA,&init_buf);
+	/*Timer1*/
 
-		GPIO_WriteBit(GPIOB,GPIO_Pin_0,Bit_SET);
-		GPIO_WriteBit(GPIOB,GPIO_Pin_1,Bit_SET);
-		GPIO_WriteBit(GPIOA,GPIO_Pin_4,Bit_RESET);
 
-		//	GPIO_Write(GPIOB,0xAA00);
+	/*End etting*/
+
+	GPIO_WriteBit(GPIOB,GPIO_Pin_0,Bit_SET);
+	GPIO_WriteBit(GPIOB,GPIO_Pin_1,Bit_SET);
+	GPIO_WriteBit(GPIOA,GPIO_Pin_4,Bit_RESET);
+
+	//	GPIO_Write(GPIOB,0xAA00);
 
   // Infinite loop
   while (1)
