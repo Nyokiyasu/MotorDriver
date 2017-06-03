54e7050a4d2bf7c2b4993c7b74330d25a166dba3
Nyoki <hibari100206@gmail.com>
2017-06-03 18:30:50 +0900

    20kHz,Duty50でモーター回った

--- MDprg_v0.2.0/src/main.c
+++ MDprg_v0.2.0/src/main.c
@@ -22,56 +22,57 @@ int main()
 
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1,ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);
 
 	/*setting*/
 	/*GPIO*/
 	/*PORTB*/
 	GPIO_StructInit(&init_gpio);
-	init_gpio.GPIO_Pin &= 0x0003;	//0,1��ݒ�
+	init_gpio.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1;	//
 	init_gpio.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_Init(GPIOB,&init_gpio);
 	/*PORTA*/
 	GPIO_StructInit(&init_gpio);
-	init_gpio.GPIO_Pin &= 0x001E;	//1,2,3,4��ݒ�
+	init_gpio.GPIO_Pin = GPIO_Pin_4|GPIO_Pin_3|GPIO_Pin_2;	//
 	init_gpio.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_Init(GPIOA,&init_gpio);
 
 	GPIO_StructInit(&init_gpio);
-	init_gpio.GPIO_Pin &= 0x0100;	//8
+	init_gpio.GPIO_Pin = GPIO_Pin_1;	//1
 	init_gpio.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_Init(GPIOA,&init_gpio);
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource1,GPIO_AF_2);
 
 	/*Timer*/
-	/*TIM1*/
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource8,GPIO_AF_2);
-	GPIO_PinLockConfig(GPIOA,GPIO_Pin_8);
-	TIM_TimeBaseStructInit(&init_tmr);
+	/*TIM2*/
+	init_tmr.TIM_Period = 100-1;
+	init_tmr.TIM_Prescaler = 4-1;
+	init_tmr.TIM_ClockDivision = TIM_CKD_DIV1;
+	init_tmr.TIM_CounterMode = TIM_CounterMode_Up;
+	init_tmr.TIM_RepetitionCounter = 0x0000;
+	TIM_TimeBaseInit(TIM2,&init_tmr);
 
-	init_tmr.TIM_Period = 100;
-	init_tmr.TIM_Prescaler = 48;
-	TIM_TimeBaseInit(TIM1,&init_tmr);
-
-	TIM_CtrlPWMOutputs(TIM1,ENABLE);	//出力するために使うかも
-	TIM_OCStructInit(&init_OC);
 	init_OC.TIM_OCMode = TIM_OCMode_PWM1;
 	init_OC.TIM_Pulse = 50;
 	init_OC.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OC1Init(TIM1,&init_OC);
+	init_OC.TIM_OCIdleState = TIM_OCIdleState_Set;
+	init_OC.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OC2Init(TIM2,&init_OC);
+	TIM_Cmd(TIM2,ENABLE);
 
-	TIM_ARRPreloadConfig(TIM1,ENABLE);
+	TIM_ARRPreloadConfig(TIM2,ENABLE);
 //	TIM_ITConfig();	//タイマ割り込みをする際に使用する
 
 
 
 	/*End setting*/
 
-	GPIO_WriteBit(GPIOA,GPIO_Pin_1,Bit_SET);
+//	GPIO_WriteBit(GPIOA,GPIO_Pin_1,Bit_SET);
 	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_SET);
-	GPIO_WriteBit(GPIOA,GPIO_Pin_3,Bit_RESET);
-	GPIO_WriteBit(GPIOA,GPIO_Pin_4,Bit_RESET);
+	GPIO_WriteBit(GPIOA,GPIO_Pin_3,Bit_SET);
 
-	GPIO_WriteBit(GPIOB,GPIO_Pin_0,Bit_RESET);
+	GPIO_WriteBit(GPIOA,GPIO_Pin_4,Bit_RESET);
+	GPIO_WriteBit(GPIOB,GPIO_Pin_0,Bit_SET);
 	GPIO_WriteBit(GPIOB,GPIO_Pin_1,Bit_SET);
 
 
