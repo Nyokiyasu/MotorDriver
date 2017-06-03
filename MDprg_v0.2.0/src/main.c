14a7810d58c7a7151dafede527400f1b050d26e3
Nyoki <hibari100206@gmail.com>
2017-06-03 15:44:39 +0900

    タイマの記述をしたけど動くかわかんない

--- MDprg_v0.2.0/src/main.c
+++ MDprg_v0.2.0/src/main.c
@@ -18,6 +18,8 @@ int main()
 
 	GPIO_InitTypeDef init_gpio;
 	TIM_TimeBaseInitTypeDef init_tmr;
+	TIM_OCInitTypeDef init_OC;
+
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1,ENABLE);
@@ -26,22 +28,34 @@ int main()
 	/*GPIO*/
 	/*PORTB*/
 	GPIO_StructInit(&init_gpio);
-	init_gpio.GPIO_Pin &= 0x0003;	//0,1��ݒ�
+	init_gpio.GPIO_Pin &= 0x0003;	//0,1��ݒ�
 	init_gpio.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_Init(GPIOB,&init_gpio);
 	/*PORTA*/
 	GPIO_StructInit(&init_gpio);
-	init_gpio.GPIO_Pin &= 0x001E;	//1,2,3,4��ݒ�
+	init_gpio.GPIO_Pin &= 0x001E;	//1,2,3,4��ݒ�
 	init_gpio.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_Init(GPIOA,&init_gpio);
+
 	/*Timer*/
 	/*TIM1*/
-//	TIM_TimeBaseStructInit(&init_tmr);
-//	TIM_TimeBaseInit(TIM1,&init_tmr);
-
 	GPIO_PinAFConfig(GPIOA,GPIO_PinSource1,GPIO_AF_2);
+	TIM_TimeBaseStructInit(&init_tmr);
+	init_tmr.TIM_Period = 100;
+	init_tmr.TIM_Prescaler = 48;
+	TIM_TimeBaseInit(TIM1,&init_tmr);
+//	TIM_CtrlPWMOutputs(TIM1,ENABLE);	//出力するために使うかも
+	TIM_OCStructInit(&init_OC);
+	init_OC.TIM_OCMode = TIM_OCMode_PWM1;
+	init_OC.TIM_OutputState = 50;
+	init_OC.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OC1Init(TIM1,&init_OC);
+//	TIM_ITConfig();	//タイマ割り込みをする際に使用する
 
-	/*End etting*/
+
+
+	/*End setting*/
+
 	GPIO_WriteBit(GPIOA,GPIO_Pin_1,Bit_SET);
 	GPIO_WriteBit(GPIOA,GPIO_Pin_2,Bit_SET);
 	GPIO_WriteBit(GPIOA,GPIO_Pin_3,Bit_RESET);
