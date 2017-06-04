c6897d43fb65cd27db0c84edcaeee0ec141ddff9
Nyoki <hibari100206@gmail.com>
2017-06-04 18:44:36 +0900

    CANの関数追加

---
+++ MDprg_v0.2.0/inc/Can.h
@@ -0,0 +1,18 @@
+﻿/*
+ * Can.h
+ *
+ *  Created on: 2017/05/21
+ *      Author: mugi111
+ */
+
+#ifndef CAN_H_
+#define CAN_H_
+
+
+void CanInit();
+void CanFilterInit(int id, int mode, int num, int scale, int fifo);
+void CanReceive(uint8_t fifo, CanRxMsg* RxStructure);
+//↓作ったはいいけど動くかわからないやつ
+uint8_t FifoCheck(uint8_t fifo);
+
+#endif /* CAN_H_ */
---
+++ MDprg_v0.2.0/src/Can.c
@@ -0,0 +1,74 @@
+﻿/*
+ * Can.c
+ *
+ *  Created on: 2017/05/21
+ *      Author: mugi111
+ */
+
+#include <stm32f0xx.h>
+#include "Can.h"
+
+
+void CanInit()
+{
+	//↓ここ適当
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+
+	CAN_InitTypeDef CAN_InitStructure;
+	CAN_InitStructure.CAN_Mode = CAN_Mode_LoopBack;
+//	CAN_InitStracture.CAN_SJW = CAN_SJW_1tq;
+	CAN_InitStructure.CAN_BS1 = CAN_BS1_4tq;
+	CAN_InitStructure.CAN_BS2 = CAN_BS2_3tq;
+	CAN_InitStructure.CAN_TTCM = DISABLE;
+	CAN_InitStructure.CAN_ABOM = DISABLE;
+	CAN_InitStructure.CAN_AWUM = DISABLE;
+	CAN_InitStructure.CAN_NART = DISABLE;
+	CAN_InitStructure.CAN_RFLM = ENABLE;
+	CAN_InitStructure.CAN_TXFP = DISABLE;
+	CAN_InitStructure.CAN_Prescaler = 128;
+
+	CAN_Init(CAN, &CAN_InitStructure);
+
+//	じーぴーあいおーのしゅつりょくせってい
+
+//	おるたねーとふぁんくしょんのせってい
+}
+
+void CanFilterInit(int id, int mode, int num, int scale, int fifo)
+{
+	//あいでぃーのぶんかつしょり　だいにゅうするための
+
+	CAN_FilterInitTypeDef FilterInitStructure;
+	FilterInitStructure.CAN_FilterActivation = ENABLE;
+	FilterInitStructure.CAN_FilterMode = mode;
+	FilterInitStructure.CAN_FilterMaskIdHigh = ;
+	FilterInitStructure.CAN_FilterMaskIdLow = ;
+	FilterInitStructure.CAN_FilterIdHigh = ;
+	FilterInitStructure.CAN_FilterIdLow = ;
+	if(num > 14)	num = 13;
+	FilterInitStructure.CAN_FilterNumber = num;
+	FilterInitStructure.CAN_FilterScale = scale;
+	FilterInitStructure.CAN_FilterFIFOAssignment = fifo;
+
+	CAN_FilterInit(&FilterInitStructure);
+}
+
+void CanReceive(uint8_t fifo, CanRxMsg* RxStructure)
+{
+	CAN_Receive(CAN, fifo, RxStructure);
+}
+
+uint8_t FifoCheck(uint8_t fifo)
+{
+	uint8_t fill;
+
+	if(fifo == 0){
+		fill = (CAN->RF0R)>>3 & 1;
+	}else{
+		fill = (CAN->RF1R)>>3 & 1;
+	}
+
+	return fill;
+}
+
+
